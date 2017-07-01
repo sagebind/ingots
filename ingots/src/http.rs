e83038effdc6e9aac6521660b0e7016899a2c0cc
Stephen M. Coakley <me@stephencoakley.com>
2017-07-01 02:22:50 -0500

    Tighten up HTTP API

--- ingots/src/http.rs
+++ ingots/src/http.rs
@@ -1,6 +1,13 @@
+//! Ingot's HTTP interface.
+//!
+//! This HTTP module is not meant to be full-featured. The API was designed to have as little surface area as possible
+//! while still being idiomatic and easy to use. This helps reduce the amount of work required for both servers and
+//! handler frameworks to implement and use the interface.
 use std::io;
 use std::net::SocketAddr;
 
+
+pub type StatusCode = u16;
 
 /// Encapsulates the state of an individual HTTP request from the web server.
 pub trait Context {
@@ -24,17 +31,17 @@ pub trait Context {
 ///
 /// Provides information about a client request, including parameters, attributes, and a request body stream.
 pub trait Request: io::Read {
-    /// Get the request URI.
-    ///
-    /// The returned URI must start with "/" and must exclude the query string portion of the URI and the "?" separator.
-    /// If the request URI cannot be determined, this method should simply return "/".
-    fn uri(&self) -> String;
-
     /// Get the HTTP request method.
     ///
     /// The string returned is not required to follow strict casing. You should normalize the returned string before
     /// checking for specific HTTP methods.
     fn method(&self) -> String;
+
+    /// Get the request URI.
+    ///
+    /// The returned URI must start with "/" and must exclude the query string portion of the URI and the "?" separator.
+    /// If the request URI cannot be determined, this method should simply return "/".
+    fn uri(&self) -> String;
 
     /// The protocol the request was made with, such as "HTTP/1.1".
     fn protocol(&self) -> String;
@@ -45,42 +52,49 @@ pub trait Request: io::Read {
     }
 
     /// Get the request headers.
-    fn headers(&self);
+    ///
+    /// Returns an array of HTTP request headers as pairs of the header name and value. Header names are not required to
+    /// have the same letter casing as sent by the client.
+    fn headers(&self) -> &[(&str, &str)];
 
-    fn get_header(&self, name: &str) -> Option<&str>;
+    /// Get the value of a request header.
+    ///
+    /// The header name is case-insensitive, and may not match the exact casing as sent by the client.
+    ///
+    /// Implementors should override this method if possible, as the default implementation does not perform very well.
+    fn get_header(&self, name: &str) -> Option<&str> {
+        let name = name.to_lowercase();
+
+        for &(header_name, value) in self.headers() {
+            if header_name.to_lowercase() == name {
+                return Some(value);
+            }
+        }
+
+        None
+    }
 
     /// Check if this is a secure HTTPS connection.
     fn is_secure(&self) -> bool {
         false
     }
-
-    /// Check if this request was made by JavaScript.
-    ///
-    /// This is typcially determined by checking the value of the `X-Requested-With` header.
-    fn is_ajax(&self) -> bool {
-        self.get_header("X-Requested-With") == Some("XMLHttpRequest")
-    }
 }
 
 /// An outgoing HTTP response.
+///
+/// Response objects are not constructed by the application; they are constructed by the web server that is proxying the
+/// response. The response is a "write-oriented" API, where content is written to the response sequentially.
 pub trait Response: io::Write {
     /// Get the response status code.
-    fn get_status(&self) -> u8;
+    fn status(&self) -> StatusCode;
 
     /// Set the response status code.
-    fn set_status(&mut self, status: u8);
+    fn set_status(&mut self, status: StatusCode);
 
-    fn headers(&self);
-    fn get_header(&self, name: &str, value: &str);
-    fn set_header(&self, name: &str, value: &str);
-
-    /// Check if the response headers have already been sent.
-    fn headers_sent(&self) -> bool;
-
-    /// Send the request headers if the have not yet been sent.
+    /// Set the value of the specified header.
     ///
-    /// This is done implicitly when the body is first written to.
-    fn send_headers(&mut self);
+    /// This method will replace any existing headers for the specified field.
+    fn set_header(&mut self, name: &str, value: String);
 
     /// Check if buffering is currently enabled for the response body.
     ///
@@ -90,33 +104,38 @@ pub trait Response: io::Write {
     /// Note that this only indicates if the response is buffered between the web server and the client. Even if
     /// buffering is disabled, there may be additional layers between the server and the user that may perform response
     /// buffering, such as proxies.
-    fn get_buffering(&self) -> bool;
+    ///
+    /// Servers are not required to implement all modes of response buffering, but they *should* return an accurate
+    /// description of how the server will handle the response body.
+    fn buffering(&self) -> Buffering;
 
     /// Request for output buffering to be enabled or disabled, returning the new buffering policy.
     ///
     /// This method does not explicitly control buffering; it is up to the web server whether responses are buffered or
-    /// not. The return value of this method is provided by the web server to indicate if changing the buffering policy
-    /// is honored.
+    /// not. The return value of this method is provided by the web server to indicate if the given buffering policy
+    /// will be honored. Servers are not required to support changing the buffering at all.
     ///
-    /// This method shall have no effect if the response body has been written partially or completely, or if headers
+    /// The buffering policy API does not offer as much control to the application as more explicit APIs do, but some
+    /// servers may not support all of the ways of handling a response body.
+    ///
+    /// This method *may* have no effect if the response body has been written partially or completely, or if headers
     /// have already been written.
     fn set_buffering(&mut self, buffering: bool) -> bool;
 
-    /// Finalize the response and send it to the client.
-    fn end(&mut self);
+    /// Check if the response headers have already been sent.
+    fn headers_sent(&self) -> bool;
 }
 
-pub struct StatusCode(pub u16);
+/// Defines a policy for buffering the content of a response.
+#[derive(Clone, Copy, Debug)]
+pub enum Buffering {
+    /// Content buffering is enabled with a maximum buffer size (in bytes).
+    On(u32),
 
-impl StatusCode {
-    // pub const OK: Self = StatusCode(200);
-}
-
-pub struct BasicRequest {
-    status: u8,
-}
-
-
-pub enum Entity {
-    Buffered(String),
+    /// Content buffering is disabled.
+    ///
+    /// Unless the `Content-Length` header is explicitly set, the web server *should* stream content written to the
+    /// response to the browser. For HTTP/1.1, the web server may use chunked transfer encoding; in HTTP/2, content
+    /// might be written to the output stream directly.
+    Off,
 }
